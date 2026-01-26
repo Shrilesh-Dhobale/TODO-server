0f88df076c8f534fa393abbf132deff2bbf69bb1
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-26 15:08:03 +0530

    Health api created

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const app = express();
 app.use(express.json());
 
 const PORT=8008;
+
+app.get('/health', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Server is healthy"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
